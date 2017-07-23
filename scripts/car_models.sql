bccb3dba8d3242405c6500d6cea9aca7bb3aa5e8
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-07-22 19:09:23 -1000

    car models missing semi colo

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,7 +9,8 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
 
 CREATE INDEX make_titles
   ON car_models (make_title);
